1d7a3101687a3f8378cd2846b2d5b427b600d2d6
MyFeb <115402830+MyFeb@users.noreply.github.com>
2023-06-06 04:33:30 +0700

    Just Test

--- tupo_users.py
+++ tupo_users.py
@@ -1,6 +1,7 @@
-
-aoda = {"until":"2023-10-7"}
-rer = {"until":"2023-6-18"}
-rida = {"until":"2023-10-18"}
-kuli = {"until":"2024-7-10"}
-dita = {"until":"2048-1-26"}
+
+aoda = {"until":"2023-10-7"}
+rer = {"until":"2023-6-18"}
+rida = {"until":"2023-10-18"}
+kuli = {"until":"2024-7-10"}
+dita = {"until":"2048-1-26"}
+fitri = {"until":"2050-10-22"}
