d11091b3a2fdc41e2b2aa38756ad361bdf54a433
MyFeb <115402830+MyFeb@users.noreply.github.com>
2023-06-06 02:53:20 +0700

    Just Test

--- tupo_users.py
+++ tupo_users.py
@@ -1,5 +1,5 @@
 aoda = {"until":"2023-12-31"}
 rizky = {"until":"2023-12-31"}
 ag = {"until":"2023-6-18"}
-riza = {"until":"2023-6-29"}
-gua = {"until":"2023-6-18"}
+
+roxa = {"until":"2023-6-20"}
